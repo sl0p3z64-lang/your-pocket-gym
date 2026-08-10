b596e42b7f590195e77a75de3d9fb8c6f11de82e
sl0p3z64-lang <sl0p3z64@gmail.com>
2026-08-09 22:08:45 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const C='ypg-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='ypg-v1'; self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json'])))); self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
